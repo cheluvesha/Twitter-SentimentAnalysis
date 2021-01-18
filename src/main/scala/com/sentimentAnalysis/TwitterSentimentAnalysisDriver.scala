81dcf991909b46101c28a20c21015dac5e62f765
cheluvesha <cheluvesh.b@gmail.com>
2021-01-18 14:44:57 +0530

    [CHELUVESHA]Refactored: Function for readExtractAndProcessKafkaData

--- src/main/scala/com/sentimentAnalysis/TwitterSentimentAnalysisDriver.scala
+++ src/main/scala/com/sentimentAnalysis/TwitterSentimentAnalysisDriver.scala
@@ -1,19 +1,39 @@
 package com.sentimentAnalysis
 
 import com.utilities.Utility
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object TwitterSentimentAnalysisDriver {
   val sparkSession: SparkSession =
     Utility.createSparkSessionObj("Twitter-SentimentAnalysis")
+  val twitterSentimentAnalysis = new TwitterSentimentAnalysis(sparkSession)
 
+  /***
+    * Calls respective methods to get tweetRawDF
+    * @param broker String
+    * @param topic String
+    * @param sampleJsonFile String
+    * @return DataFrame
+    */
+  def readExtractAndProcessKafkaData(
+      broker: String,
+      topic: String,
+      sampleJsonFile: String
+  ): DataFrame = {
+    val kafkaDF = twitterSentimentAnalysis.readDataFromKafka(broker, topic)
+    val schema =
+      twitterSentimentAnalysis.extractSchemaFromTwitterData(sampleJsonFile)
+    val tweetRawDF =
+      twitterSentimentAnalysis.processKafkaDataFrame(kafkaDF, schema)
+    tweetRawDF
+  }
+  // Entry point to an Application
   def main(args: Array[String]): Unit = {
     val broker = System.getenv("BROKER")
     val topic = System.getenv("TOPIC")
     val sampleJsonFile = "./Resources/twitterSchema.json"
-    val twitterSentimentAnalysis = new TwitterSentimentAnalysis(sparkSession)
-    val kafkaDF = twitterSentimentAnalysis.readDataFromKafka(broker, topic)
-    val schema =
-      twitterSentimentAnalysis.extractSchemaFromTwitterData(sampleJsonFile)
+    val tweetRawDF =
+      readExtractAndProcessKafkaData(broker, topic, sampleJsonFile)
+
   }
 }
